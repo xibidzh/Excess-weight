a4b1e10c91ee371a6e95bbeaf72a18b82c670056
xibidzh <89549734+xibidzh@users.noreply.github.com>
2022-02-09 16:41:17 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,23 @@
+let yourHeight = prompt('Введите свой рост в метрах (0.0):');
+let yourWeight = prompt('Введите свой вес в кг:');
+let isMassOver = yourWeight/(yourHeight*yourHeight);
+
+if (isMassOver <= 19) {
+    alert('У Ваш  дефицит веса');
+}    
+else if (isMassOver <= 26) {
+    alert('Ваш вес в норме');
+} 
+else if (isMassOver >= 30){
+    alert('У Ваш ожирение I степени');
+}
+else if (isMassOver >= 26){
+    alert('Ваш вес больше нормы');
+}
+console.log(isMassOver);
+
+
+
+
+
+
